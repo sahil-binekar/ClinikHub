9e3185eb893708fb604e551bee9d61cd63288813
sahil-binekar <sahilbinekar@gmail.com>
2025-03-27 16:35:42 +0530

    Implemented Registration and login module with JWT Auth

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  # enum :role, { doctor: "doctor", receptionist: "receptionist" }
+  # enum :role, { doctor: 0, receptionist: 1 }
+  enum :role, [:doctor, :receptionist]
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+end
---
+++ db/migrate/20250327092045_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password_digest
+      t.string :first_name
+      t.string :last_name
+      t.string :phone
+      t.integer :role
+
+      t.timestamps
+    end
+  end
+end
